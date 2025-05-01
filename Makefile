ef133c884c92df8fdf630657ccd04b2c7e77cc06
Jahir <93404854+JahirWH@users.noreply.github.com>
2025-04-30 22:40:37 -0600

    base

---
+++ Makefile
@@ -0,0 +1,19 @@
+.PHONY: all clean run
+
+all: os.iso
+
+kernel.bin: kernel.c
+	i686-elf-gcc -ffreestanding -c kernel.c -o kernel.o
+	i686-elf-gcc -ffreestanding -nostdlib -T linker.ld kernel.o -o kernel.bin
+
+os.iso: kernel.bin grub.cfg
+	mkdir -p iso/boot/grub
+	cp kernel.bin iso/boot/
+	cp grub.cfg iso/boot/grub/
+	grub-mkrescue -o os.iso iso
+
+clean:
+	rm -rf *.o *.bin os.iso iso
+
+run: os.iso
+	qemu-system-i386 -cdrom os.iso
---
+++ kernel.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include <stdint.h>
+
+// Prototipo para escribir en la pantalla
+void print(const char *str) {
+    uint16_t *VideoMemory = (uint16_t *)0xB8000;
+    for (size_t i = 0; str[i] != '\0'; i++) {
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    }
+}
+
+// Función principal del kernel
+void kernel_main(void) {
+    print("Cargando Python...\n");
+
+    // Aquí se debería cargar un intérprete de Python
+    // Para un sistema real, necesitarías integrar un intérprete embebido como MicroPython.
+    while (1) {}
+}
